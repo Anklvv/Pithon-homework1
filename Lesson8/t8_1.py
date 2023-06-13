f6be32a166ffe206ddb4c615c81bcf26f7e80825
annushka <annikolaeva0211@mail.ru>
2023-06-13 20:40:06 +0300

    chng

---
+++ Lesson8/t8_1.py
@@ -0,0 +1,41 @@
+all_data = []
+last_id = 0
+file_base = base.txt
+
+
+def read_records():
+        pass
+
+def show_all():
+        pass
+
+
+def main_menu():
+    play = True
+    while play:
+        answer = input("Phone book:\n")
+                        "1. Show all records\n"
+                        "2. Add a record\n"
+                        "3. Search a  record\n"
+                        "4. Change \n"
+                        "5. Delete \n"
+                        "6. Imr\Exp \n"
+                        "7. Exit \n"
+        match answer:
+                case "1":
+                        show_all()
+                case "2":
+                        pass
+                case "3":
+                        pass
+                case "4":
+                        pass
+                case "5":
+                        pass
+                case "6":
+                        pass
+                case "7":
+                        pass
+         
+        
+        
