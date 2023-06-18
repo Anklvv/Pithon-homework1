1807f4b92c134e2d5768b1e5016bc8629e74d65c
annushka <annikolaeva0211@mail.ru>
2023-06-18 19:18:58 +0300

    chng

--- Lesson8/t8_1.py
+++ Lesson8/t8_1.py
@@ -3,143 +3,124 @@
 # Задача 38: Дополнить телефонный справочник возможностью изменения и удаления данных. Пользователь также может ввести имя или фамилию, и Вы должны реализовать функционал для изменения и удаления данных
 # from os import path
 
-# file_base = "base.txt"
-# last_id = 0
-# all_data = []
-
-# if not path.exists(file_base):
-#     with open(file_base, "w", encoding="utf-8") as _:
-#         pass
-
-
-# def read_records():
-#     global last_id, all_data
-
-#     with open(file_base, encoding="utf-8") as f:
-#         all_data = [i.strip() for i in f]
-#         if all_data:
-#             last_id = int(all_data[-1].split()[0])
-#             return all_data
-#     return []
-
-
-# def show_all():
-#     if all_data:
-#         print(*all_data, sep="\n")
-#     else:
-#         print("Empty data")
-
-
-# def add_new_contact():
-#     global last_id
-#     array = ["surname", "name", "patronymic", "phone_number"]
-#     string = ""
-#     for i in array:
-#         string += input(f"Enter {i} ") + " "
-#     last_id += 1
-
-#     with open(file_base, "a", encoding="utf-8") as f:
-#         f.write(f"{last_id} {string}\n")
-
-
-# def main_menu():
-#     play = True
-#     while play:
-#         read_records()
-#         answer = input("Phone book:\n"
-#                        "1. Show all records\n"
-#                        "2. Add a record\n"
-#                        "3. Search a record\n"
-#                        "4. Change\n"
-#                        "5. Delete\n"
-#                        "6. Exp/Imp\n"
-#                        "7. Exit\n")
-#         match answer:
-#             case "1":
-#                 show_all()
-#             case "2":
-#                 add_new_contact()
-#             case "3":
-#                 pass
-#             case "4":
-#                 pass
-#             case "5":
-#                 play = False
-#             case _:
-#                 print("Try again!\n")
-
-
-# main_menu()
-
-
 from os import path
 
-all_data = []
+file_base = "base.txt"
 last_id = 0
-file_base = base.txt
+all_data = []
 
 if not path.exists(file_base):
-       with open(file_base, "w", encoding="utf-8") as f:
-              pass
-              
+    with open(file_base, "w", encoding="utf-8") as _:
+        pass
 
 
 def read_records():
-        global all_data, last_id
+    global last_id, all_data
 
-        with open(file_base, encoding="utf-8") as f: 
-                all_data = [i.strip() for i in f ]
-                if all_data:
-                       last_id..................
-                last_id = int(all_data[-1].split()[0])
-        return all_data
-
-
- def show_all():
-         if all_data:
-            print(*all_data)
-        else:
-            print("Empty data!\n")
+    with open(file_base, encoding="utf-8") as f:
+        all_data = [i.strip() for i in f]
+        if all_data:
+            last_id = int(all_data[-1].split()[0])
+            return all_data
+    return []
 
 
-def add_new_record():
-       global last_id
+
+def show_all():
+    if all_data:
+        print(*all_data, sep="\n")
+    else:
+        print("Empty data")
+
+  
 
 
+def add_new_contact():
+    global last_id
     array = ["surname", "name", "patronymic", "phone_number"]
     string = ""
-
     for i in array:
-       string += input(f"Enter {i} ") + " "
+        string += input(f"Enter {i} ") + " "
+    last_id += 1
+
+    with open(file_base, "a", encoding="utf-8") as f:
+        f.write(f"{last_id} {string}\n")
+
+
+
+def delete_data(file_base):
+    with open(file_base, "r", encoding="utf-8") as f:
+        tel_book = f.read()
+        print(tel_book)
+    index_delete_data = int(input("Введите номер строки для удаления: ")) - 1
+    tel_book_lines = tel_book.split("\n")
+    del_tel_book_lines = tel_book_lines[index_delete_data]
+    tel_book_lines.pop(index_delete_data)
+    print(f"Удалена запись: {del_tel_book_lines}\n")
+    with open(file_base, "w", encoding='utf-8') as f:
+        f.write("\n".join(tel_book_lines))
+
+
+
+
+def edit_data(filename):
+    with open(filename, "r", encoding='utf-8') as data:
+        tel_book = data.read()
+    print(tel_book)
+    print("")
+    index_change_data = int(input("Введите номер строки для редактирования: ")) - 1
+    tel_book_lines = tel_book.split("\n")
+    edit_tel_book_lines = tel_book_lines[index_change_data]
+    elements = edit_tel_book_lines.split()
+    print(elements)
+    surname = input("Введите фамилию: ")
+    name = input("Введите имя: ")
+    patronymic = input("Введите отчество: ")
+    phone = input("Введите номер телефона: ")
+    num = elements[0]
+    if len(surname) == 0:
+        surname = elements[1]
+    if len(name) == 0:
+        name = elements[2]
+    if len(patronymic) == 0:
+        patronymic = elements[3]
+    if len(phone) == 0:
+        phone = elements[4]
+    tel_book_lines[index_change_data] = num + ' ' + surname + ' ' + name + ' ' + patronymic + ' ' + phone
+    with open(filename, "w", encoding='utf-8') as f:
+        f.write("\n".join(tel_book_lines))
 
 
 
 def main_menu():
     play = True
     while play:
-        answer = input("Phone book:\n")
-        "1. Show all records\n"
-        "2. Add a record\n"
-        "3. Search a  record\n"
-        "4. Change \n"
-        "5. Delete \n"
-        "6. Imr\Exp \n"
-        "7. Exit \n"
+        read_records()
+        answer = input("Phone book:\n"
+                       "1. Show all records\n"
+                       "2. Add a record\n"
+                       "3. Search a record\n"
+                       "4. Change\n"
+                       "5. Delete\n"
+                       "6. Exp/Imp\n"
+                       "7. Exit\n")
         match answer:
-                case "1":
-                    show_all()
-                case "2":
-                    add_new_record()
-                case "3":
-                        pass
-                case "4":
-                        pass
-                case "5":
-                        pass
-                case "6":
-                        pass
-                case "7":
-                        pass
-         
-        
-        
+            case "1":
+                show_all()
+            case "2":
+                add_new_contact()
+            case "3":
+                pass
+            case "4":
+                edit_data(file_base)
+            case "5":
+                delete_data(file_base)
+            case "6":
+                pass
+            case "7":
+                play = False
+            case _:
+                print("Try again!\n")
+
+
+main_menu()
